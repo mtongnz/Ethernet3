10ba5d13501fc7e109d1f092be681020e34c44e6
Matt <mtongnz@users.noreply.github.com>
2018-06-05 14:17:13 +1200

    Speed up SPI ?
    Hoping to make the SPI a bit faster

--- src/utility/w5500.cpp
+++ src/utility/w5500.cpp
@@ -21,7 +21,7 @@
 W5500Class w5500;
 
 // SPI details
-SPISettings wiznet_SPI_settings(8000000, MSBFIRST, SPI_MODE0);
+SPISettings wiznet_SPI_settings(20000000, MSBFIRST, SPI_MODE0);
 uint8_t SPI_CS;
 
 void W5500Class::init(uint8_t socketNumbers, uint8_t ss_pin)
